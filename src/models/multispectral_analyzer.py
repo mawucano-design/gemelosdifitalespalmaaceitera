fe6abe66f26ed44c4db9175157cf6eb12d22b2dd
mawucano-design <mawucano@gmail.com>
2025-12-12 18:30:01 -0300

    Create multispectral_analyzer.py

---
+++ src/models/multispectral_analyzer.py
@@ -0,0 +1,10 @@
+def analyze_multispectral_data(tree_id: str, bands: dict):
+    """
+    STUB: Calcula NDVI, NDRE, clorofila, estrés hídrico, etc.
+    bands = {'red': ..., 'nir': ..., 'green': ..., 'red_edge': ...}
+    """
+    return {
+        "ndvi": 0.7,
+        "ndre": 0.3,
+        "stress_index": 0.1
+    }
